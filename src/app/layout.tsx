a09e47591a2430256130b03f112696d6e2e86564
NhanHcmue <email@example.com>
2025-08-08 14:11:20 +0700

    fix layout, and delete file bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,6 @@ export const metadata = { title: 'Loop Hit Music', description: 'Independent pho
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <head>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js" strategy="beforeInteractive" />
-        <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.birds.min.js" strategy="beforeInteractive" />
-      </head>
       <body className="min-h-screen flex flex-col">
         <main className="flex-grow">
           {children}
