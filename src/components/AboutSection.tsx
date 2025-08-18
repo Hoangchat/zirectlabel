28a731251d546ce1c59abe6dff62ef1208f0f28a
Hoangchat <jayzidrive@gmail.com>
2025-08-19 05:27:39 +0700

    image

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -5,21 +5,20 @@ import ScrollReveal from './ScrollReveal';
 import Link from 'next/link';
 
 const playlistData = [
-  { src: "/images/PLL1.png", followers: "+4.000 FOLLOWERS PLAYLIST.", url: "https://open.spotify.com/playlist/3T7iY2hYQJLB3JKubnkXjl" },
-  { src: "/images/222.png", followers: "+20.000.000 PLAYS ON SOUNDCLOUD.", url: "https://soundcloud.com/" },
-  { src: "/images/3333.png", followers: "+14.000 FOLLOWERS PLAYLIST.", url: "https://open.spotify.com/playlist/15haiEJWHeyZDWLoti9sF5" }
+  { src: "./images/PLL1.png", followers: "+4.000 FOLLOWERS PLAYLIST.", url: "https://open.spotify.com/playlist/3T7iY2hYQJLB3JKubnkXjl" },
+  { src: "./images/222.png", followers: "+20.000.000 PLAYS ON SOUNDCLOUD.", url: "https://soundcloud.com/" },
+  { src: "./images/3333.png", followers: "+14.000 FOLLOWERS PLAYLIST.", url: "https://open.spotify.com/playlist/15haiEJWHeyZDWLoti9sF5" }
 ];
 
 const youtubeChannels = [
-  { src: "/images/SLAVA FUNK 4.0!.jpg", url: "https://www.youtube.com/watch?v=rjsZeY1uT-w&list=OLAK5uy_nPAu-y87pF_0ITMB-JZIWcVBWyiEKOZYI&index=4", title: "SLAVA FUNK 4.0!", description: "uniwes, KW1K" },
-  { src: "/images/DANÇA TAKA.png", url: "https://www.youtube.com/watch?v=AyzYpUnS_ks&list=OLAK5uy_kU9-PAvPWjMzsithRWVKsbSGwVkS2dBWA&index=2", title: "DANÇA TAKA", description: "FAB10, DJZPX" },
-  { src: "/images/Portão do Céu.png", url: "https://www.youtube.com/watch?v=654BZYpX9fU&list=OLAK5uy_kF_W1fcLW4r_hQ1YjTlmJ97daOM0WRpCc&index=2", title: "Portão do Céu", description: "KW1K, FAB10" },
-  { src: "/images/TUNG SAHUR FUNK.jpg", url: "https://www.youtube.com/watch?v=uT0Xsxj0SQw&list=OLAK5uy_kpWCYcEQfsO-bXsK59ix5ow84iWHJcJfg&index=1", title: "TUNG SAHUR FUNK", description: "KW1K" },
-  { src: "/images/1 B!T.png", url: "https://www.youtube.com/watch?v=FVidVx2QJLc&list=OLAK5uy_lQ0egE06EPfDknUNfN1XPkapGvUkMRVbE&index=3", title: "1 B!t", description: "DJ FiK, uniwes" },
-  { src: "/images/unnamed.jpg", url: "https://www.youtube.com/watch?v=nNXHzermhtQ&list=OLAK5uy_lQidNdKlqYj3Fz3FzOsdLoOtlCQ67Gh-w&index=3", title: "TRALALERO TRALALA FUNK", description: "KW1K" },
-  { src: "/images/FLAME AURA.jpg", url: "https://www.youtube.com/watch?v=JpUGZA1_pAw&list=OLAK5uy_mAYkrRg7HEhhega96pIlf1XOmKVZV3aOs&index=4", title: "FLAME AURA", description: "uniwes, FAB10" },
-  { src: "/images/Novo Mundo.png", url: "https://www.youtube.com/watch?v=-4MZm1Rwh4c&list=OLAK5uy_kj_yeAkTQBSQF4680b5zr9jQOGMQy64mI&index=3", title: "Novo Mundo", description: "uniwes" },
-  { src: "https://i.pinimg.com/1200x/e6/34/d3/e634d384fb0c31d7245d70d6f70f830d.jpg", url: "https://www.youtube.com/watch?v=-4MZm1Rwh4c&list=OLAK5uy_kj_yeAkTQBSQF4680b5zr9jQOGMQy64mI&index=3", title: "Novo Mundo", description: "uniwes" },
+  { src: "./images/SLAVA FUNK 4.0!.jpg", url: "https://www.youtube.com/watch?v=rjsZeY1uT-w&list=OLAK5uy_nPAu-y87pF_0ITMB-JZIWcVBWyiEKOZYI&index=4", title: "SLAVA FUNK 4.0!", description: "uniwes, KW1K" },
+  { src: "./images/DANÇA TAKA.png", url: "https://www.youtube.com/watch?v=AyzYpUnS_ks&list=OLAK5uy_kU9-PAvPWjMzsithRWVKsbSGwVkS2dBWA&index=2", title: "DANÇA TAKA", description: "FAB10, DJZPX" },
+  { src: "./images/Portão do Céu.png", url: "https://www.youtube.com/watch?v=654BZYpX9fU&list=OLAK5uy_kF_W1fcLW4r_hQ1YjTlmJ97daOM0WRpCc&index=2", title: "Portão do Céu", description: "KW1K, FAB10" },
+  { src: "./images/TUNG SAHUR FUNK.jpg", url: "https://www.youtube.com/watch?v=uT0Xsxj0SQw&list=OLAK5uy_kpWCYcEQfsO-bXsK59ix5ow84iWHJcJfg&index=1", title: "TUNG SAHUR FUNK", description: "KW1K" },
+  { src: "./images/1 B!T.png", url: "https://www.youtube.com/watch?v=FVidVx2QJLc&list=OLAK5uy_lQ0egE06EPfDknUNfN1XPkapGvUkMRVbE&index=3", title: "1 B!t", description: "DJ FiK, uniwes" },
+  { src: "./images/unnamed.jpg", url: "https://www.youtube.com/watch?v=nNXHzermhtQ&list=OLAK5uy_lQidNdKlqYj3Fz3FzOsdLoOtlCQ67Gh-w&index=3", title: "TRALALERO TRALALA FUNK", description: "KW1K" },
+  { src: "./images/FLAME AURA.jpg", url: "https://www.youtube.com/watch?v=JpUGZA1_pAw&list=OLAK5uy_mAYkrRg7HEhhega96pIlf1XOmKVZV3aOs&index=4", title: "FLAME AURA", description: "uniwes, FAB10" },
+  { src: "./images/Novo Mundo.png", url: "https://www.youtube.com/watch?v=-4MZm1Rwh4c&list=OLAK5uy_kj_yeAkTQBSQF4680b5zr9jQOGMQy64mI&index=3", title: "Novo Mundo", description: "uniwes" },
 ];
 
 export default function AboutSection() {
