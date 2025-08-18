f648d442fcc91e3065172c50d90c34eeda04e020
Hoangchat <jayzidrive@gmail.com>
2025-08-19 04:30:42 +0700

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
         {/* Left: Logo */}
         <Link href="/" className="text-2xl font-bold text-blue-600 dark:text-white">
           <Image
-            src="/logo.png"
+            src="../public/logo.png"
             alt="Logo"
             width={100}
             height={40}
